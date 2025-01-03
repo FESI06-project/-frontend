0987abcde96e7328f96d043b61523582c7e237f6
심은주 <nyaknya@naver.com>
2025-01-03 15:26:34 +0900

    ✨ feat: 로딩 컴포넌트 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <>
-      <header>임시헤더</header>
-      <main>메인</main>
-      <footer>임시푸터</footer>
-    </>
-  );
+  return <div>리스트 페이지~</div>;
 }
