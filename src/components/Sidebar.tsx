c37d857f6d26a75eb57615ba974c977473d46c25
sumin305 <gogotnals@naver.com>
2025-01-06 15:47:59 +0900

    ✨ feat: SideBar content 구현

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,6 +5,15 @@ import { useState } from 'react';
 
 export default function SideBar() {
   const { isListExpanded } = useLayoutStore();
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const sideBarContents = ['모임 찾기', '찜한 모임', '모든 방명록'];
+  const mypageContents = ['마이페이지', '로그아웃'];
+  const isActive = (isActive: boolean) => {
+    if (isActive) {
+      return `text-primary flex items-center text-lg w-[252px] h-[50px] p-[15px] bg-dark-300 rounded-[10px]`;
+    }
+    return `text-white flex items-center text-lg w-[252px] h-[50px] p-[15px]`;
+  };
   return (
     <>
       {isListExpanded && (
@@ -19,6 +28,27 @@ export default function SideBar() {
           <Link href="/" className="text-red-500 font-bold text-2xl mb-[30px]">
             FitMon
           </Link>
+          <ul className="flex flex-col gap-y-4 justify-center">
+            {sideBarContents.map((content, index) => (
+              <li
+                key={index}
+                className={`${isActive(index === selectedIndex)}`}
+              >
+                {content}
+              </li>
+            ))}
+          </ul>
+          <p className="text-white text-sm mt-[50px] ml-[23px]">{'MYPAGE'}</p>
+          <ul className="flex flex-col gap-y-4 justify-center">
+            {mypageContents.map((content, index) => (
+              <li
+                key={index}
+                className={`${isActive(index + 3 === selectedIndex)}`}
+              >
+                {content}
+              </li>
+            ))}
+          </ul>
         </div>
       )}
     </>
