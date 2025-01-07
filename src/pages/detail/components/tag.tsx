c984b74dcf5322a5327ea64c7916677ecf7b1d28
sumin305 <gogotnals@naver.com>
2025-01-07 13:23:18 +0900

    ✨ feat: 모임 태그 컴포넌트 구현

---
+++ src/pages/detail/components/tag.tsx
@@ -0,0 +1,26 @@
+interface TagProps {
+  tagName: string;
+}
+
+interface TagListProps {
+  tagList: string[];
+}
+export function Tag({ tagName }: TagProps) {
+  return (
+    <div className="px-[20px] py-[10px] bg-dark-200 flex justify-center items-center rounded-full">
+      <p className="text-primary text-base font-semibold">{`#${tagName}`}</p>
+    </div>
+  );
+}
+
+export default function TagList({ tagList }: TagListProps) {
+  return (
+    <>
+      <div className="flex gap-2">
+        {tagList.map((tag, index) => (
+          <Tag key={index} tagName={tag} />
+        ))}
+      </div>
+    </>
+  );
+}
