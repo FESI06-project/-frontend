f2fdb5d84e7ccfa3f04a90b1252608f6246968c9
sumin305 <gogotnals@naver.com>
2025-01-07 13:23:46 +0900

    ✨ feat: 모임 시간, 장소 기능 구현

--- src/pages/detail/[gatheringId].tsx
+++ src/pages/detail/[gatheringId].tsx
@@ -1,17 +1,20 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import TagList from './\bcomponents/tag';
 
 export default function GatheringDetail() {
   const router = useRouter();
   const gatheringId = router.query.gatheringId;
   const gathering = {
     gatheringTitle: '모임 제목',
-    gatheringDescription: '모임 설명',
+    gatheringDescription:
+      '디스크립션은50자까지 올수있습니답둘셋넷 디스크립션은50자까지 올수있습니답둘셋넷 ',
     ownerStatus: true, // 이 사용자가 모임장인지 아닌지
     gatheringImage: 'www.www.ww.w.w.w.w',
     gatheringMainType: '유산소형',
     gatheringSubType: '런닝',
-    gatheringTags: ['심심할 때', '스트레스 풀면서 달리기', '런닝 최고'],
+    gatheringTags: ['심심할 때', '스트레스', '런닝 최고'],
     gatheringStartDate: '2022-22-22',
     gatheringEndDate: '2022-23-23',
     gatheringSi: '대전',
@@ -73,12 +76,17 @@ export default function GatheringDetail() {
       },
     ],
   };
+
+  useEffect(() => {
+    console.log(gatheringId);
+  }, []);
+
   return (
-    <div className="bg-dark-100 w-[1200px] flex place-self-center">
-      <div id="gathering-information" className="">
+    <div className="w-[1200px] flex place-self-center">
+      <div id="gathering-information" className="w-full">
         <div id="type-information">
           <div className="flex mt-20 gap-[10px]">
-            <p className="text-white">{gathering.gatheringMainType}</p>
+            <p>{gathering.gatheringMainType}</p>
             <Image
               src="/assets/image/arrow-right.svg"
               alt="arrow"
@@ -94,19 +102,46 @@ export default function GatheringDetail() {
             height={300}
             alt="gathering-image"
             src="/assets/image/fitmon.png"
-            className="rounded-[20px] mr-[50px]"
+            className="rounded-[20px] mr-[50px] w-[280px] h-[300px] object-cover"
           />
-          <div id="detail-information">
-            <h1>{gathering.gatheringTitle}</h1>
-            <p>{gathering.gatheringDescription}</p>
-            <div id="tags">
-              {gathering.gatheringTags.map((tag) => (
-                <span key={tag}>{`#${tag} `}</span>
-              ))}
+          <div id="detail-information" className=" w-full">
+            <h3 className="text-[1.75rem] font-semibold">
+              {gathering.gatheringTitle}
+            </h3>
+            <p className="text-[1.125rem] text-dark-700 mt-4">
+              {gathering.gatheringDescription}
+            </p>
+            <div id="tags" className="mt-5">
+              <TagList tagList={gathering.gatheringTags} />
             </div>
-            <div id="range-and-place">
-              <div id="range">{`${gathering.gatheringStartDate}~${gathering.gatheringEndDate}`}</div>
-              <div id="place">{`${gathering.gatheringSi} ${gathering.gatheringGu}`}</div>
+            <div
+              id="range-and-place"
+              className="w-full mt-[25px] py-[30px] bg-dark-200 rounded-[20px]"
+            >
+              <div id="range" className="flex items-center mb-[13px]">
+                <Image
+                  src="/assets/image/time.svg"
+                  width={14}
+                  height={14}
+                  alt="time"
+                  className="ml-[25px] mr-2"
+                />
+                <h1 className="font-semibold">{'모임 기간'}</h1>
+                <p className="bg-dark-500 h-[12px] w-[1px] mx-[15px]"></p>{' '}
+                <p>{`${gathering.gatheringStartDate}~${gathering.gatheringEndDate}`}</p>
+              </div>
+              <div id="place" className="flex items-center">
+                <Image
+                  src="/assets/image/place.svg"
+                  width={14}
+                  height={14}
+                  alt="place"
+                  className="ml-[25px] mr-2"
+                />
+                <h1 className="font-semibold">{'모임 장소'}</h1>
+                <p className="bg-dark-500 h-[12px] w-[1px] mx-[15px]"></p>
+                <p>{`${gathering.gatheringSi} ${gathering.gatheringGu}`}</p>
+              </div>
             </div>
           </div>
         </div>
