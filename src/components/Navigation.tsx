6bcf03bd432a196d2f17341a30774024cc58cd34
sumin305 <gogotnals@naver.com>
2025-01-07 13:22:57 +0900

    🎨 design: global css 폰트 배수 삭제

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import useMemberStore from '@/stores/useMemberStore';
 import useLayoutStore from '@/stores/useLayoutStore';
+import { useEffect } from 'react';
 
 export default function Navigation() {
   const router = useRouter();
@@ -21,7 +22,7 @@ export default function Navigation() {
   };
 
   return (
-    <nav className=" sticky top-0 left-0 w-full bg-dark-100 shadow-lg z-40">
+    <nav className="sticky top-0 left-0 w-full bg-dark-100 shadow-lg z-40">
       <div className="container mx-auto px-6 lg:px-10">
         <div className="flex items-center justify-between h-20">
           {/* 로고 영역 */}
@@ -37,10 +38,7 @@ export default function Navigation() {
                 height="20"
               />
             </div>
-            <Link
-              href="/"
-              className="text-red-500 font-bold text-[2.8rem] ml-6"
-            >
+            <Link href="/" className="text-red-500 font-bold text-2xl ml-6">
               FitMon
             </Link>
           </div>
@@ -50,19 +48,19 @@ export default function Navigation() {
             <div className="flex items-center space-x-8">
               <Link
                 href="/meeting"
-                className={`${isActive('/meeting')} hover:text-red-500 px-3 py-2 text-[1.6rem] font-medium transition-colors`}
+                className={`${isActive('/meeting')} hover:text-red-500 px-3 py-2 text-base font-medium transition-colors`}
               >
                 모임 찾기
               </Link>
               <Link
                 href="/popular"
-                className={`${isActive('/popular')} hover:text-red-500 px-3 py-2 text-[1.6rem] font-medium transition-colors`}
+                className={`${isActive('/popular')} hover:text-red-500 px-3 py-2 text-base font-medium transition-colors`}
               >
                 평한 모임
               </Link>
               <Link
                 href="/all"
-                className={`${isActive('/all')} hover:text-red-500 px-3 py-2 text-[1.6rem] font-medium transition-colors`}
+                className={`${isActive('/all')} hover:text-red-500 px-3 py-2 text-base font-medium transition-colors`}
               >
                 모든 방명록
               </Link>
@@ -95,7 +93,7 @@ export default function Navigation() {
                       </div>
                     </button>
                   </div>
-                  <div className="hidden md:flex items-center text-gray-300 text-[1.6rem]">
+                  <div className="hidden md:flex items-center text-gray-300 text-sm">
                     {nickname}
                   </div>
                 </div>
@@ -104,7 +102,7 @@ export default function Navigation() {
           ) : (
             <button
               onClick={() => setIsLogin(!isLogin)}
-              className="w-[100px] h-[34px] rounded-[10px]  text-[1.6rem] bg-primary text-white"
+              className="sm:w-[124px] w-[100px] h-[34px] rounded-[10px] text-base bg-primary text-white"
             >
               {'로그인'}
             </button>
