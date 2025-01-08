918417ab638e6cbd2a8b2dc4e32791fe429abc35
sumin305 <gogotnals@naver.com>
2025-01-08 10:20:07 +0900

    ✨ feat: Heart 공통 컴포넌트 구현

---
+++ src/components/Heart.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+
+export default function Heart({ rating }: { rating: number }) {
+  const fill = Math.floor(rating);
+  const half = rating % 1;
+
+  return (
+    <div id="hearts" className="flex">
+      {[...Array(fill)].map((_, index) => (
+        <Image
+          className="mr-[5px]"
+          src="/assets/image/heart-fill.svg"
+          alt="heart"
+          width={22}
+          height={22}
+          key={index}
+        />
+      ))}
+      {half > 0 && (
+        <Image
+          src="/assets/image/heart-half.svg"
+          alt="heart"
+          width={22}
+          height={22}
+        />
+      )}
+    </div>
+  );
+}
