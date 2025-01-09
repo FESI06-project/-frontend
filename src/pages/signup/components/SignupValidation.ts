5609b2a22f8a2b5e1b7c2f39860dd59015bff321
moonjw0 <jjwon1230@naver.com>
2025-01-09 13:45:13 +0900

    ✨ feature: 회원가입 유효성 검사 컴포넌트 분리

---
+++ src/pages/signup/components/SignupValidation.ts
@@ -0,0 +1,62 @@
+export interface RegisterValidationProps {
+  e: React.FocusEvent<HTMLInputElement>;
+  password: string;
+  setFormDataError: React.Dispatch<
+    React.SetStateAction<{
+      email: boolean;
+      nickName: boolean;
+      password: boolean;
+      passwordCheck: boolean;
+    }>
+  >;
+}
+
+// 회원가입 유효성 검사
+export function RegisterValidation({
+  e,
+  password,
+  setFormDataError,
+}: RegisterValidationProps) {
+  const { name, value } = e.target;
+
+  // 닉네임 유효성 검사
+  const NicknameValidation = () => {
+    setFormDataError((prev) => ({
+      ...prev,
+      [name]: value.length < 2 || value.length > 10,
+    }));
+  };
+
+  // 이메일 유효성 검사
+  const EmailValidation = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    setFormDataError((prev) => ({
+      ...prev,
+      [name]: !emailRegex.test(value),
+    }));
+  };
+  // 비밀번호 유효성 검사
+  const PasswordValidation = () => {
+    setFormDataError((prev) => ({
+      ...prev,
+      [name]: value.length < 8,
+    }));
+  };
+  // 비밀번호 확인 검사
+  const PasswordCheckValidation = () => {
+    setFormDataError((prev) => ({
+      ...prev,
+      [name]: value.length == 0 || value !== password,
+    }));
+  };
+
+  if (name === 'nickName') {
+    NicknameValidation();
+  } else if (name === 'email') {
+    EmailValidation();
+  } else if (name === 'password') {
+    PasswordValidation();
+  } else if (name === 'passwordCheck') {
+    PasswordCheckValidation();
+  }
+}
