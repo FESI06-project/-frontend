fe07c0bd02c7631f0e16167015b81748de3cbef4
sumin305 <gogotnals@naver.com>
2025-01-09 11:19:52 +0900

    ✨ feat: 모달 띄울 root 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,11 @@ import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <aside id="aside-root"></aside>
+    </>
   );
 }
