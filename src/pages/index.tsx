41dbdb28c73a20dc9dc1767da9d8508bf68b3f54
심은주 <nyaknya@naver.com>
2025-01-10 16:31:24 +0900

    ✨ feat: 메인 - 챌린지 레이아웃 완성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,11 @@ import ListChallenge from '@/pages/main/components/ListChallenge';
 
 export default function Home() {
   return (
-    <div>
-      <h2>지금 핫한 챌린지 🔥</h2>
-      <div>
+    <div className="max-w-screen-xl mx-auto px-8 pt-20">
+      <h2 className="text-[1.75rem] font-semibold pb-[30px]">
+        지금 핫한 챌린지 🔥
+      </h2>
+      <div className="overflow-hidden">
         <ListChallenge />
       </div>
     </div>
--- src/pages/main/components/ChallengeCard.tsx
+++ src/pages/main/components/ChallengeCard.tsx
@@ -1,5 +1,7 @@
 import { MainChallenge } from '@/types';
 import Link from 'next/link';
+import getDatePart from '@/utils/getDatePart';
+import Image from 'next/image';
 
 interface ChallengeCardProps {
   data: MainChallenge;
@@ -17,11 +19,36 @@ export default function ChallengeCard({ data }: ChallengeCardProps) {
     successParticipantCount,
   } = data;
 
-  const date = `${startDate} ~ ${endDate}`;
+  const date = `${getDatePart(startDate)} ~ ${getDatePart(endDate)}`;
 
   return (
-    <Link href={`/detail/${gatheringId}`}>
-      <div>{date}</div>
+    <Link
+      href={`/detail/${gatheringId}`}
+      className="w-[320px] h-[320px] flex flex-col justify-between relative rounded-2xl overflow-hidden bg-dark-200 p-[30px]"
+    >
+      <div className="flex flex-col gap-2.5 relative z-10">
+        <div className="text-sm text-dark-700">{date}</div>
+        <h3 className="text-2xl font-semibold">{title}</h3>
+        <p className=" text-dark-700">{description}</p>
+      </div>
+      <div className="flex justify-between relative z-10">
+        <div className="flex items-center font-normal gap-2 text-white">
+          <span className=" font-semibold">
+            {participantCount} / {successParticipantCount}
+          </span>
+          <Image
+            src="/assets/image/person.svg"
+            alt="참여자 아이콘"
+            width={22}
+            height={22}
+          />
+        </div>
+        <div className="font-semibold">챌린지 참여하기 →</div>
+      </div>
+      <div
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        className="bg-cover blur-[2px] absolute left-0 top-0 w-full h-full opacity-20 z-0"
+      ></div>
     </Link>
   );
 }
