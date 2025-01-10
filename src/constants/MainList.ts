0e886f56a2e93af91258addb1fd17cd7b8b50135
심은주 <nyaknya@naver.com>
2025-01-10 16:59:27 +0900

    ✨ feat: 메인 페이지 탭 추가

---
+++ src/constants/MainList.ts
@@ -0,0 +1,18 @@
+export const LISTPAGE_MAINTYPE = [
+  {
+    id: '전체',
+    label: '전체',
+  },
+  {
+    id: '유산소형',
+    label: '유산소형',
+  },
+  {
+    id: '무산소형',
+    label: '무산소형',
+  },
+  {
+    id: '경기형',
+    label: '경기형',
+  },
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
+import Tab from '@/components/common/Tab';
+import { LISTPAGE_MAINTYPE } from '@/constants/MainList';
 import ListChallenge from '@/pages/main/components/ListChallenge';
+import { useState } from 'react';
 
 export default function Home() {
+  const [currentTab, setCurrentTab] = useState('전체');
+
   return (
     <div className="max-w-screen-xl mx-auto px-8 pt-20">
       <h2 className="text-[1.75rem] font-semibold pb-[30px]">
@@ -8,6 +13,13 @@ export default function Home() {
       </h2>
       <div className="overflow-hidden">
         <ListChallenge />
+      </div>
+      <div className="mt-20">
+        <Tab
+          items={LISTPAGE_MAINTYPE}
+          currentTab={currentTab}
+          onTabChange={(newTab) => setCurrentTab(newTab)}
+        />
       </div>
     </div>
   );
