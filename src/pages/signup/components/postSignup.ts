19087233cdb4d12ded70f5062e4b2e949b82ee2d
songyeonji <dus3439@naver.com>
2025-01-10 15:14:36 +0900

    🐛 fix : 빌드오류 컨프릭 수정

--- src/pages/signup/components/postSignup.ts
+++ src/pages/signup/components/postSignup.ts
@@ -1,16 +1,8 @@
 import axiosInstance from '@/utils/axios';
-<<<<<<< HEAD
-
-interface postSignupProps {
-  email: string;
-  nickName: string;
-  password: string;
-=======
 import router from 'next/router';
 
 interface postSignupProps {
   message: string;
->>>>>>> ccbdc5fca1066179f4fd665919849ac451985686
 }
 
 const postSignup = async (data: {
@@ -19,23 +11,14 @@ const postSignup = async (data: {
   password: string;
 }) => {
   try {
-<<<<<<< HEAD
-    console.log('data', data);
-    console.log(JSON.stringify(data));
-=======
->>>>>>> ccbdc5fca1066179f4fd665919849ac451985686
     const response = await axiosInstance.post<postSignupProps>(
       'api/v1/signup',
       data,
     );
-<<<<<<< HEAD
-    console.log('response', response.data);
-=======
     if (response.data.message === '사용자 생성 성공') {
       console.log('회원가입 성공');
       router.push('/login');
     }
->>>>>>> ccbdc5fca1066179f4fd665919849ac451985686
   } catch (error) {
     console.error('회원가입 요청 에러:', error);
   }
