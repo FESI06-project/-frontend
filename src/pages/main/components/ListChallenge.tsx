8ba4206fcd00074b079100e3d7125cfbe1d766f7
심은주 <nyaknya@naver.com>
2025-01-10 15:20:48 +0900

    🔥 remove: 스테이트 사용할필요 없어서 삭제

--- src/pages/main/components/ListChallenge.tsx
+++ src/pages/main/components/ListChallenge.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react';
 import { MainChallenge } from '@/types/index';
 import { useQuery } from '@tanstack/react-query';
 import apiRequest from '@/utils/apiRequest';
 
 export default function ListChallenge() {
-  const [challenges, setChallenges] = useState<MainChallenge[] | null>([]);
-
   const { data } = useQuery<MainChallenge[]>({
     queryKey: ['mainChallengeList'],
     queryFn: async () => {
