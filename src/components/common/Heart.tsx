32b4d19261c18bdd6809173e44d7ff5c3f78e77c
sumin305 <gogotnals@naver.com>
2025-01-10 13:50:24 +0900

    ✨ feat: Heart 공통 컴포넌트 수정

--- src/components/common/Heart.tsx
+++ src/components/common/Heart.tsx
@@ -2,11 +2,11 @@ import Image from 'next/image';
 export default function Heart({ rating }: { rating: number }) {
   const fill = Math.floor(rating);
   const half = rating % 1;
+  const emptyCount = 5 - fill - (half > 0 ? 1 : 0);
   return (
-    <div id="hearts" className="flex">
+    <div id="hearts" className="flex gap-[5px]">
       {[...Array(fill)].map((_, index) => (
         <Image
-          className="mr-[5px]"
           src="/assets/image/heart-fill.svg"
           alt="heart"
           width={22}
@@ -22,6 +22,16 @@ export default function Heart({ rating }: { rating: number }) {
           height={22}
         />
       )}
+      {[...Array(emptyCount)].map((_, index) => (
+        <Image
+          className="mr-[5px]"
+          src="/assets/image/heart-empty.svg"
+          alt="heart"
+          width={22}
+          height={22}
+          key={index}
+        />
+      ))}
     </div>
   );
 }
