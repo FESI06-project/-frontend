562391963305b4f910f83757c9b6ee43a700b100
moonjw0 <jjwon1230@naver.com>
2025-01-13 23:47:08 +0900

    ✨ feat: input 유효성 검사용 useDebounce

---
+++ src/utils/validation/useDebounce.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+export default function useDebounce<T>(value: T, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    // 지연시간 지난 후 값 업데이트
+    const timeoutId = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      // 값이 바뀌거나 언마운트되면 기존 타이머 취소
+      clearTimeout(timeoutId);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
