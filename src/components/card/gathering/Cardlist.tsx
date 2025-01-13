d88eca121a38092c6ef78b078d4e1adb8bba3508
심은주 <nyaknya@naver.com>
2025-01-13 15:20:19 +0900

    ✨ featr: 메인타입에 따른 서브태그 종류 변경

--- src/components/card/gathering/Cardlist.tsx
+++ src/components/card/gathering/Cardlist.tsx
@@ -2,28 +2,26 @@ import Loading from '@/components/dialog/Loading';
 import { GatheringList } from '@/types';
 import apiRequest from '@/utils/apiRequest';
 import { useQuery } from '@tanstack/react-query';
+import { MainType } from '@/constants/MainList';
 import Card from './Card';
 
 interface CardlistProps {
-  mainType: '전체' | '유산소형' | '무산소형' | '경기형';
+  mainType: MainType;
+  subType: string; // subType은 문자열로 받음
 }
 
-export default function Cardlist({ mainType }: CardlistProps) {
+export default function Cardlist({ mainType, subType }: CardlistProps) {
   const param =
     mainType === '전체'
       ? '/api/v1/gatherings?sortBy=deadline&sortDirection=ASC&page=0&pageSize=6'
-      : `/api/v1/gatherings?sortBy=deadline&sortDirection=ASC&page=0&pageSize=6&mainType=${mainType}`;
+      : `/api/v1/gatherings?sortBy=deadline&sortDirection=ASC&page=0&pageSize=6&mainType=${mainType}&subType=${subType}`;
 
   const { data, isLoading } = useQuery<GatheringList>({
-    queryKey: ['gatheringList', mainType],
+    queryKey: ['gatheringList', mainType, subType], // subType 포함
     queryFn: async () => {
-      return await apiRequest<GatheringList>({
-        param,
-      });
+      return await apiRequest<GatheringList>({ param });
     },
   });
-
-  console.log(data);
 
   if (isLoading) {
     return <Loading />;
@@ -31,10 +29,7 @@ export default function Cardlist({ mainType }: CardlistProps) {
 
   return (
     <div>
-      {data &&
-        data.content.map((gathering) => {
-          return <Card key={gathering.gatheringId} />;
-        })}
+      {data?.content.map((gathering) => <Card key={gathering.gatheringId} />)}
     </div>
   );
 }
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
         />
       </div>
       <div className="mt-7">
-        <Cardlist mainType={mainType} />
+        <Cardlist mainType={mainType} subType={subType} />
       </div>
     </div>
   );
