85630442489bbcdaea3f57ca45f6ebbffbae2a79
심은주 <nyaknya@naver.com>
2025-01-13 17:09:49 +0900

    ✨ feat: 넥스트 컨피그 이미지 url 설정

--- src/components/card/gathering/Card.tsx
+++ src/components/card/gathering/Card.tsx
@@ -1,3 +1,56 @@
-export default function Card() {
-  return <div>카드 컴포넌트</div>;
+import { GatheringListItem } from '@/types';
+import getDatePart from '@/utils/getDatePart';
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface CardProps {
+  data: GatheringListItem;
+}
+
+export default function Card({ data }: CardProps) {
+  console.log(data);
+
+  const {
+    gatheringId,
+    title,
+    imageUrl,
+    subType,
+    startDate,
+    endDate,
+    minCount,
+    totalCount,
+    participantCount,
+    status,
+    tags,
+    mainLocation,
+    subLocation,
+  } = data;
+
+  const date = `${getDatePart(startDate)} ~ ${getDatePart(endDate)}`;
+
+  return (
+    <Link href={`/detail/${gatheringId}`}>
+      <div>
+        {/* 스테이터스 임시태그 */}
+        <div>{status}</div>
+        <Image src={imageUrl} width={220} height={220} alt="모임 사진"></Image>
+      </div>
+      <div>
+        <span>
+          {subType} | {mainLocation} {subLocation}
+        </span>
+        <h2>{title}</h2>
+        <span>{date}</span>
+        <div>
+          {tags.map((tag) => {
+            return <div key={tag}>{tag}</div>;
+          })}
+        </div>
+        <div>
+          {/* 임시 */}
+          {minCount} {totalCount} {participantCount}
+        </div>
+      </div>
+    </Link>
+  );
 }
