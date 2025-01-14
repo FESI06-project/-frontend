bcb4bd9af47b3e182f30de7ac5e5f539b7a52415
moonjw0 <jjwon1230@naver.com>
2025-01-15 00:45:47 +0900

    ✨ feat: 로그인 요청 함수 타입 정의

--- src/pages/login/components/postLogin.ts
+++ src/pages/login/components/postLogin.ts
@@ -2,30 +2,24 @@ import useMemberStore from '@/stores/useMemberStore';
 import axiosInstance from '@/utils/axios';
 import router from 'next/router';
 
-interface postLoginProps {
+export interface postLoginProps {
   email: string;
-  memberId: number;
+  password: string;
 }
 
-const postLogin = async (data: { email: string; password: string }) => {
-  try {
-    const response = await axiosInstance.post<postLoginProps>(
-      'api/v1/login',
-      data,
-    );
-    console.log('response', response.data);
+export interface postLoginResponse {
+  memberId: number;
+  nickName: string;
+  email: string;
+  profileImageUrl: string | null;
+}
 
-    if (response.data.memberId) {
-      localStorage.setItem('isLogin', 'true');
-      localStorage.setItem('nickname', 'test');
-      localStorage.setItem('email', response.data.email.toString());
-      localStorage.setItem('memberId', response.data.memberId.toString());
-      useMemberStore.getState().setIsLogin(true);
-      router.push('/');
-    }
-  } catch (error) {
-    console.error('로그인 요청 에러:', error);
-  }
-};
-
-export default postLogin;
+export default async function postLogin(
+  data: postLoginProps,
+): Promise<postLoginResponse> {
+  const response = await axiosInstance.post<postLoginProps>(
+    'api/v1/login',
+    data,
+  );
+  return response.data;
+}
